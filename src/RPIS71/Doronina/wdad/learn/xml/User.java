2ef1072098183a77748c9577170fbfa130061090
SarahJewers <Goldilocks.13.a@gmail.com>
2019-09-15 21:51:10 +0400

    иногда происходит иногда всегда происходит иногда

---
+++ src/RPIS71/Doronina/wdad/learn/xml/User.java
@@ -0,0 +1,7 @@
+package RPIS71.Doronina.wdad.learn.xml;
+
+public class User {
+    private String mail;
+    private String name;
+    private String rights;
+}
--- src/RPIS71/Doronina/wdad/learn/xml/XmlTask.java
+++ src/RPIS71/Doronina/wdad/learn/xml/XmlTask.java
@@ -1,4 +1,40 @@
 package RPIS71.Doronina.wdad.learn.xml;
 
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.input.DOMBuilder;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
 public class XmlTask {
+
+    private Document document;
+    private String path;
+
+    public XmlTask(String path) throws IOException, SAXException, ParserConfigurationException {
+        this.path = path;
+
+        File inputFile = new File(path);
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        org.w3c.dom.Document doc = dBuilder.parse(inputFile);
+
+        DOMBuilder domBuilder = new DOMBuilder();
+        document = domBuilder.build(doc);
+    }
+
+    public String getNoteText (User owner, String title) {
+
+        Element root = document.getRootElement();
+        List<Element> childs = root.getChildren("note");
+    }
 }
