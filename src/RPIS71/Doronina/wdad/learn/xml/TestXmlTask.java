0ea5f47b8ca7a45e8921783645112fd58ce7422a
SarahJewers <Goldilocks.13.a@gmail.com>
2019-10-09 05:05:25 +0400

    жабы мои жабы

--- src/RPIS71/Doronina/wdad/learn/xml/TestXmlTask.java
+++ src/RPIS71/Doronina/wdad/learn/xml/TestXmlTask.java
@@ -6,21 +6,6 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 
 public class TestXmlTask {
-    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
-        XmlTask test = new XmlTask("src/PO62/Suslov/wdad/learn/xml/test.xml");
 
-        User user = new User("test@yandex.ru", "test");
 
-        test.updateNote(user, "Еда", "Сделать покушать 2.0");
-
-        System.out.println(test.getNoteText(user, "Еда"));
-
-        XmlTask test2 = new XmlTask("src/PO62/Suslov/wdad/learn/xml/test2.xml");
-
-        User user2 = new User("user1@yandex.ru", "user");
-
-        test2.setPrivileges(user2, "Лабы", 3);
-
-        System.out.println(test2.getNoteText(user2, "Лабы"));
-    }
 }
--- src/RPIS71/Doronina/wdad/learn/xml/User.java
+++ src/RPIS71/Doronina/wdad/learn/xml/User.java
@@ -3,41 +3,5 @@ package RPIS71.Doronina.wdad.learn.xml;
 import java.util.Objects;
 
 public class User {
-    private String mail;
-    private String name;
-    private String rights;
 
-    public User(String mail, String name) {
-        this(mail, name, "R");
-    }
-
-    public User(String mail, String name, String rights) {
-        this.mail = mail;
-        this.name = name;
-        this.rights = rights;
-    }
-
-    public String getMail() {
-        return mail;
-    }
-
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getRights() {
-        return rights;
-    }
-
-    public void setRights(String rights) {
-        this.rights = rights;
-    }
 }
